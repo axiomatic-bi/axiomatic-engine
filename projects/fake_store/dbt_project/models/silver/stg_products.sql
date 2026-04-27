6ec9516f6a451716d4fbe56a50a7cb045be745cf
amisont <134074972+amisont@users.noreply.github.com>
2026-04-27 11:37:21 +0100

    Refactor category and name fields in staging models for improved formatting
    - Updated `stg_products.sql` to apply `initcap` and `trim` functions to the `category` field for consistent capitalization and whitespace handling.
    - Modified `stg_users.sql` to apply `initcap` and `trim` functions to `name__firstname` and `name__lastname` fields for better presentation of user names.

--- projects/fake_store/dbt_project/models/silver/stg_products.sql
+++ projects/fake_store/dbt_project/models/silver/stg_products.sql
@@ -7,7 +7,7 @@ select
     cast(id as bigint) as product_id,
     cast(title as varchar) as title,
     cast(description as varchar) as description,
-    cast(category as varchar) as category,
+    initcap(trim(cast(category as varchar))) as category,
     cast(image as varchar) as image,
     cast(price as double) as price,
     cast(rating__rate as double) as rating_rate,
